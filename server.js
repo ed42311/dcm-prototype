f757dd6d9a58fefcbdf581b2f2e9cee5ba3524c2
ed42311 <ed42311@gmail.com>
2017-12-26 18:35:22 -0700

    added Procfile and vars for port

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var path = require('path');
 var express = require('express');
-
 var app = express();
 
-var staticPath = path.join(__dirname, '/public');
-app.use(express.static(staticPath));
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(3000, function() {
-  console.log('listening');
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
